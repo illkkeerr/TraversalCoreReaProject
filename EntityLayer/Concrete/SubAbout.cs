3c428ca99dd0387e3cffb2a9bfcf9f39d2ef712c
İlker Şenel <ilkersenel5797@gmail.com>
2023-10-30 12:44:32 +0300

    Entityler oluşturuldu. Core paketleri projelere yüklendi

---
+++ EntityLayer/Concrete/SubAbout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class SubAbout
+	{
+		public int SubAboutId { get; set; }
+		public int Title { get; set; }
+		public int Description { get; set; }
+	}
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Testimonial
+	{
+		public int TestimonialId { get; set; }
+		public int Client { get; set; }
+		public int Comment { get; set; }
+		public int ClientImage { get; set; }
+		public int Status { get; set; }
+	}
+}
