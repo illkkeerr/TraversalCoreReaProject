a88859cd28f52d2afc8eb3b818fecba79067b8e7
İlker Şenel <ilkersenel5797@gmail.com>
2023-12-11 21:24:21 +0300

    yeni modeller eklendi

--- TraversalCoreRea/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreRea/Areas/Member/Controllers/ProfileController.cs
@@ -1,12 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreRea.Areas.Member.Models;
 
 namespace TraversalCoreRea.Areas.Member.Controllers
 {
+    [Area("Member")]
+    [Route("Member/{controller}/{action}")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+
+            userEditViewModel.Name = values.Name;
+            userEditViewModel.SurName = values.Surname;
+            userEditViewModel.PhoneNumber = values.PhoneNumber;
+            userEditViewModel.Mail = values.Email;
+
+
+            return View(userEditViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Image.FileName);
+                var imagename = Guid.NewGuid() + extension;
+                var savelocation = resource + "/wwwroot/userimages/" + imagename;
+                var stream=new FileStream(savelocation, FileMode.Create);
+                await p.Image.CopyToAsync(stream);
+
+                user.ImageUrl=imagename;
+            }
+
+            user.Name=p.Name;
+            user.Surname = p.SurName;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
+            var result=await _userManager.UpdateAsync(user);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
             return View();
+
         }
     }
 }
---
+++ TraversalCoreRea/Models/UserSignInModel.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreRea.Models
+{
+	public class UserSignInModel
+	{
+		[Required(ErrorMessage="Lütfen kullanıcı adını giriniz.")]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Lütfen şifrenizi giriniz.")]
+		public string Password { get; set; }
+
+	}
+}
